a6afd743cb39b346ea08032ecac4338ea8260079
Dimitry Zykin <119896089+DimitryZykin@users.noreply.github.com>
2023-05-12 21:45:21 +0400

    Update User.java

--- StudentServiceApp/src/StudentDomen/User.java
+++ StudentServiceApp/src/StudentDomen/User.java
@@ -1,42 +1,89 @@
 package StudentDomen;
 
-public class User {
-    private String firstName;
-    private String secondName;
-    private int age;
+/**
+ * Класс, представляющий пользователя системы.
+ */
 
-    public User(String firstName, String secondName, int age) {
+public class User{
+
+    private String firstName; 
+    private String secondName;
+    private int age; 
+
+    /**
+    * Конструктор класса User.
+    * @param firstName Имя пользователя
+    * @param secondName Фамилия пользователя
+    * @param age Возраст пользователя
+    */
+
+    public User(String firstName, String secondName, int age){
         this.firstName = firstName;
         this.secondName = secondName;
         this.age = age;
     }
 
-    public String getFirstName() {
+    /**
+     * Метод для получения имени пользователя.
+     * @return Имя пользователя
+     */
+
+    public String getFirstName(){
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    /**
+     * Метод для установки имени пользователя.
+     * @param firstName Имя пользователя
+     */
+
+    public void setFirstName(String firstName){
         this.firstName = firstName;
     }
 
-    public String getSecondName() {
+    /**
+     * Метод для получения фамилии пользователя.
+     * @return Фамилия пользователя
+     */
+
+    public String getSecondName(){
         return secondName;
     }
 
-    public void setSecondName(String secondName) {
+    /**
+     * Метод для установки фамилии пользователя.
+     * @param secondName Фамилия пользователя
+     */
+
+    public void setSecondName(String secondName){
         this.secondName = secondName;
     }
 
-    public int getAge() {
+    /**
+     * Метод для получения возраста пользователя.
+     * @return Возраст пользователя
+     */
+
+    public int getAge(){
         return age;
     }
 
-    public void setAge(int age) {
+    /**
+     * Метод для установки возраста пользователя.
+     * @param age Возраст пользователя
+     */
+
+    public void setAge(int age){
         this.age = age;
     }
 
+     /**
+     * Переопределение метода toString() для вывода информации о пользователе.
+     * @return Информация о пользователе
+     */
+    
     @Override
-    public String toString() {
+    public String toString(){
         return "User{" +
                 "firstName='" + firstName + '\'' +
                 ", secondName='" + secondName + '\'' +
