c12085c82d4bdfbe24a543cdf898cfe6131607b7
Dimitry Zykin <119896089+DimitryZykin@users.noreply.github.com>
2023-05-12 21:38:52 +0400

    Update EmploeeController.java

--- StudentServiceApp/src/Controllers/EmploeeController.java
+++ StudentServiceApp/src/Controllers/EmploeeController.java
@@ -4,21 +4,44 @@ import StudentDomen.Emploee;
 import StudentDomen.User;
 import StudentService.EmploeeService;
 
-public class EmploeeController implements iUserController<Emploee> {
+/**
+ * Контроллер для работы с сотрудниками.
+ * Реализует интерфейс iUserController.
+ */
+
+public class EmploeeController implements iUserController<Emploee>{
     private final EmploeeService empService = new EmploeeService();
 
+     /**
+     * Создание нового сотрудника.
+     * @param firstName имя сотрудника
+     * @param secondName фамилия сотрудника
+     * @param age возраст сотрудника
+     * @param address адрес сотрудника
+     */
+
     @Override
-    public void create(String firstName, String secondName, int age) {
+    public void create(String firstName, String secondName, int age){
         empService.create(firstName, secondName, age);
     }
 
-    static public <T extends Emploee> void paySalary(T person)
-    {
+     /**
+     * Метод для выплаты зарплаты сотруднику.
+     * @param person сотрудник, которому выплачивается зарплата
+     */
+
+    public static <T extends Emploee> void paySalary(T person){
         System.out.println(((User) person).getFirstName()+" зп 10000р ");
     }
 
-    static public <T extends Number> Double mean(T[] num)
-    {
+     /**
+     * Метод для вычисления среднего значения числового массива.
+     * @param num массив чисел
+     * @param <T> тип элементов массива
+     * @return среднее значение элементов массива
+     */
+    
+    public static <T extends Number> Double mean(T[] num){
         double sum = 0.0;
         for(int i =0; i<num.length;i++)
         {
