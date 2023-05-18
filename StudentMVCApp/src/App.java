b30bf8a412b1fa9322ea11dcf5d29db26f8fe8ba
Dimitry Zykin <119896089+DimitryZykin@users.noreply.github.com>
2023-05-18 23:35:12 +0400

    Update App.java

--- StudentMVCApp/src/App.java
+++ StudentMVCApp/src/App.java
@@ -1,53 +1,87 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import Controller.Controller;
 import Controller.iGetModel;
 import Controller.iGetView;
 import Model.FileRepo;
 import Model.Model;
-
+import Model.ModelHashMap;
 import Model.Student;
 import View.View;
+import View.ViewEng;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+// Главный класс приложения.
 
-       List<Student> students = new ArrayList<Student>();
-       Student s1 = new Student("Сергей", "Иванов", 21, (long)101);
-       Student s2 = new Student("Андрей", "Сидоров", 22, (long)111);
-       Student s3 = new Student("Иван", "Петров", 22, (long)121);
-       Student s4 = new Student("Игорь", "Иванов", 23, (long)301);
-       Student s5 = new Student("Даша", "Цветкова", 25, (long)171);
-       Student s6 = new Student("Лена", "Незабудкина", 23, (long)104);
-       students.add(s1);
-       students.add(s2);
-       students.add(s3);
-       students.add(s4);
-       students.add(s5);
-       students.add(s6);
+public class App{
 
-       
-       FileRepo fileRepo = new FileRepo("StudentDb.txt");
-    //    for(Student pers :students)
-    //    {
-    //     fileRepo.addStudent(pers);
-    //    }
-    //    fileRepo.saveAllStudentToFile();
+   /**
+    * Создание объекта сканера для считывания пользовательского ввода с консоли
+    */
 
+   public static Scanner sc = new Scanner(System.in, "Cp866");
 
-       iGetModel model = new Model(students);
-       iGetModel modelFileRepo = fileRepo;
+   /**
+    * Возвращает объект отображения в зависимости от выбранного языка.
+    * @param lang выбранный язык
+    * @return объект отображения
+    * @throws IllegalArgumentException если выбран неподдерживаемый язык
+    */
 
-       
+   public static iGetView view(String lang){
+      iGetView view;
+      if (lang.equals("RUS")){
+         view = new View();
+      } else if (lang.equals("ENG")){
+         view = new ViewEng();
+      }else{
+         throw new IllegalArgumentException("Неподдерживаемый язык!");
+      }
+      return view;
+   }
 
+   /**
+    * Метод main() - точка входа в приложение.
+    * @param args аргументы командной строки
+    * @throws Exception если возникла ошибка
+    */
 
+   public static void main(String[] args) throws Exception{
+      System.out.printf("Выберите язык, RUS/ENG: ");
+      String lang = sc.nextLine().toUpperCase();
 
+      // Создаем базу данных студентов
 
-       iGetView view = new View();
-       Controller control = new Controller(view, modelFileRepo);
-       control.run();
-      // control.updateView();
-       
-    }
+      ModelHashMap studentsHash = new ModelHashMap();
+      studentsHash.addStudent("Екатерина", "Стриженова ", 24, 102);
+      studentsHash.addStudent("Филипп", "Киркоров ", 22, 101);
+      studentsHash.addStudent("Георгий", "Щенников ", 22, 105);
+      studentsHash.addStudent("Казимир", "Малевич ", 23, 102);
+      studentsHash.addStudent("Евгений", "Евстигнеев ", 22, 101);
+      studentsHash.addStudent("Марина", "Максимова ", 20, 102);
+
+      List<Student> studentsList = new ArrayList<Student>(Arrays.asList(
+            new Student("Екатерина", "Стриженова ", 24, 102),
+            new Student("Филипп", "Киркоров ", 22, 101),
+            new Student("Георгий", "Щенников ", 22, 105),
+            new Student("Казимир", "Малевич ", 23, 102),
+            new Student("Евгений", "Евстигнеев ", 22, 101),
+            new Student("Марина", "Максимова ", 20, 102)));
+
+      FileRepo fileRepo = new FileRepo("StudentDb.txt"); // подключаем FileRepo
+      for (Student person : studentsList){
+         fileRepo.addStudent(person);
+      }
+      fileRepo.saveAllStudentsToFile();
+
+      iGetView view = view(lang);
+      iGetModel modelHashMap = studentsHash;
+      iGetModel modelList = new Model(studentsList);
+      iGetModel modelFileRepo = fileRepo;
+      
+      Controller control = new Controller(view, modelHashMap, lang);
+      control.run();
+   }
 }
