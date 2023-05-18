1bcf14f7fe9ba470f6db0a07e9c421663e5c5d8a
Dimitry Zykin <119896089+DimitryZykin@users.noreply.github.com>
2023-05-18 23:26:10 +0400

    Update iGetView.java

--- StudentMVCApp/src/Controller/iGetView.java
+++ StudentMVCApp/src/Controller/iGetView.java
@@ -3,7 +3,25 @@ package Controller;
 import java.util.List;
 import Model.Student;
 
-public interface iGetView {
-    void printAllStudent(List<Student> students);
+/**
+ * Интерфейс(договор между программистами) iGetView.
+ * Определяет методы для отображения данных в программе.
+ */
+
+public interface iGetView{
+    
+    /**
+     * Метод printAllStudents печатает список студентов. 
+     * @param students Список студентов.
+     */
+
+    void printAllStudents(List<Student> students);
+    
+    /**
+     * Метод prompt запрашивает у пользователя ввод данных и возвращает введенное значение.
+     * @param message Сообщение, которое будет выведено перед запросом ввода данных.
+     * @return Введенное значение.
+     */
+
     String prompt(String message);
 }
