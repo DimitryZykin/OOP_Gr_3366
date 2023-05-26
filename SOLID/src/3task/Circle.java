bccadffd6680872c1bcc8a7dafedfd6e08ba04a3
Dimitry Zykin <119896089+DimitryZykin@users.noreply.github.com>
2023-05-27 00:21:56 +0400

    Create Circle.java

---
+++ SOLID/src/3task/Circle.java
@@ -0,0 +1,14 @@
+package 3task;
+
+public class Circle implements ShapeArea {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return 2 * 3.14 * radius;
+    }
+}
